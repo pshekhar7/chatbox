012195981d55a65c9741f73a3625266b84688840
Pratik Shekhar <pratik.shekhar@hyperface.co>
2023-11-19 01:41:03 +0530

    dockerized changes

--- src/main/resources/data-init.sql
+++ src/main/resources/data-init.sql
@@ -1,1 +1,7 @@
+CREATE TABLE IF NOT EXISTS app_config (
+                                        name VARCHAR(64),
+                                        value VARCHAR(255),
+                                        PRIMARY KEY (name)
+);
+
 INSERT IGNORE INTO app_config(name, value) values('pii-field-encryption', '{"password":"y371bhwr8evtcijp4560zxkmq2ladngf","iv":"8079896567784743"}');
